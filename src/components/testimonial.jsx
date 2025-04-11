0c9577a50b9f30400d8fc1bf441af7209951285f
Nathan <nathanblee@gmail.com>
2025-04-10 18:49:33 -0700

    updated testimonials (wip)

---
+++ src/components/testimonial.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./testimonial.css";
+
+const Testimonial = () => {
+  return (
+    <React.Fragment>
+      <div className="testimonial-wrapper">
+        {/* Slack Screenshots */}
+        <div className="slack-screenshots">
+          <img
+            src="/img/jess1.png"
+            alt="This is the best thing that has ever happened to me"
+          />
+          <img src="/img/slack2.png" alt="Slack convo 2" />
+          <img src="/img/slack3.png" alt="Slack convo 3" />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Testimonial;
